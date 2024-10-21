36f6c3195f2d206a366b94d695532144fe513cfd
Roman Ruiz-Esparza <2654709+rjruizes@users.noreply.github.com>
2024-10-21 10:03:22 -0400

    2.10 plugin interfaces

--- ckanext/summarystats/plugin.py
+++ ckanext/summarystats/plugin.py
@@ -19,16 +19,16 @@ class SummarystatsPlugin(plugins.SingletonPlugin):
     # IResourceController                                                 #
     # Hooks into a dataset's resource lifecycle                           #
     #######################################################################
-    def after_create(self, context, resource):
+    def after_resource_create(self, context, resource):
         enqueue_stats_job(resource)
 
     # Runs before a resource has been updated
-    def before_update(self, context, current, resource):
+    def before_resource_update(self, context, current, resource):
         context["file_uploaded"] = False
         if resource.get("upload", False):
             context["file_uploaded"] = True
 
     # Runs after a resource has been updated
-    def after_update(self, context, resource):
+    def after_resource_update(self, context, resource):
         if context.get("file_uploaded"):
             enqueue_stats_job(resource)
